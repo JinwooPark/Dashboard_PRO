605449560c2f7735bfb1c6479e1e8a48744ca4cd
Jinwoo (Jin) Park <jin@Jinwooui-noteubug.local>
2026-09-22 16:23:46 +0900

    Fix map tiles without API key

--- main.js
+++ main.js
@@ -223,10 +223,9 @@ function initMap() {
 
     if (!map) {
         map = L.map('map').setView(center, 4);
-        L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-            subdomains: 'abcd',
-            maxZoom: 20
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            maxZoom: 19
         }).addTo(map);
     } else {
         // Clear existing markers
